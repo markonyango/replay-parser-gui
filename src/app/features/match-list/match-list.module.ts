78c17050bea2d018832f68e2d282037e1770ef31
Mark Onyango <mark.onyango@gmail.com>
2022-12-01 17:28:54 +0100

    Setup base workspace structure for the frontend

---
+++ src/app/features/match-list/match-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatchListComponent } from './match-list.component';
+
+
+
+@NgModule({
+  declarations: [MatchListComponent],
+  exports: [MatchListComponent],
+})
+export class MatchListModule { }
