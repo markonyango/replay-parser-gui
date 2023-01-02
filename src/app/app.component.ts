2a154c93b66f75d3e15f573530c1cd1a9730b756
Mark Onyango <mark.onyango@gmail.com>
2023-01-02 01:25:16 +0000

    Implement Updater functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { UpdateService } from './core/services/update.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private _updateService: UpdateService) {}
+  ngOnInit() {
+    this._updateService.check();
+  }
+}
