019e699ed93dc17d8074e648f321046bb0229684
Mark Onyango <mark.onyango@gmail.com>
2025-09-06 14:23:07 +0200

    chore: upgrade to Angular v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatchListModule } from './features/match-list/match-list.module';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
 
 @NgModule({
   declarations: [
