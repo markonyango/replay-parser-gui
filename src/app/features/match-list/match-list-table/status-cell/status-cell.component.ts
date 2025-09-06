84eafd08554418a1d4725b22a2571869af5e707e
Mark Onyango <mark.onyango@gmail.com>
2025-09-06 23:15:51 +0000

    fix: parse json in status cell renderer prior to checking response status

--- src/app/features/match-list/match-list-table/status-cell/status-cell.component.ts
+++ src/app/features/match-list/match-list-table/status-cell/status-cell.component.ts
@@ -1,23 +1,31 @@
-import { Component, signal } from "@angular/core";
-import { MatIconModule } from "@angular/material/icon";
-import { ICellRendererAngularComp } from "ag-grid-angular";
-import { ICellRendererParams } from "ag-grid-community";
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ICellRendererParams } from 'ag-grid-community';
 
 @Component({
-  template: `
-    @if (ok()) { <mat-icon>check</mat-icon> } @else { <mat-icon>warning</mat-icon> }
-  `,
-  imports: [MatIconModule],
-  standalone: true
+    template: `
+        @if (ok()) {
+            <mat-icon>check</mat-icon>
+        } @else {
+            <mat-icon>warning</mat-icon>
+        }
+    `,
+    imports: [MatIconModule],
+    standalone: true,
 })
 export class StatusCellComponent implements ICellRendererAngularComp {
     agInit(params: ICellRendererParams<any, any, any>): void {
-      this.ok.set(params.value?.status?.response == 'ok');
+        console.log(params.value);
+        if (params.value?.status) {
+            const status = JSON.parse(params.value?.status);
+            this.ok.set(status.response == 'ok');
+        }
     }
 
     refresh(params: ICellRendererParams<any, any, any>): boolean {
-      return false;
+        return false;
     }
 
-    ok = signal<boolean | undefined>(undefined);
+    ok = signal<boolean>(false);
 }
